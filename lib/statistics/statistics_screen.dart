754e9d419249c73af1ecbbfb38daf9110400f01a
Alisher <aisroilov1029@gmail.com>
2024-05-06 21:08:17 +0500

    statistics decided to useanother function

--- lib/statistics/statistics_screen.dart
+++ lib/statistics/statistics_screen.dart
@@ -1,8 +1,8 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:money_manager/data/models/transaction_record.dart';
 import 'package:money_manager/home/cubit/home_cubit.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class StatisticsScreen extends StatefulWidget {
   const StatisticsScreen({super.key});
@@ -12,7 +12,10 @@ class StatisticsScreen extends StatefulWidget {
 }
 
 class _StatisticsScreenState extends State<StatisticsScreen> {
-  int touchedIndex = -1;
+  Widget buildIncomeStatisticsScreen(
+      List<TransactionRecord> transactionRecords) {
+    return;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,7 +60,7 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
                   child: TabBarView(
                     children: [
                       buildIncomeStatisticsScreen(state.transactionRecords),
-                      buildExpenseStatisticsScreen(state.transactionRecords)
+                      buildIncomeStatisticsScreen(state.transactionRecords)
                     ],
                   ),
                 );
@@ -77,139 +80,11 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
       ),
     );
   }
+}
 
-  Widget buildExpenseStatisticsScreen(
-      List<TransactionRecord> transactionRecords) {
-    Widget content = const Center(child: Text("No records yet"));
-
-    if (transactionRecords
-        .where((transactionRecord) =>
-            transactionRecord.recordType == RecordType.expense)
-        .isNotEmpty) {
-      content = PieChart(
-        PieChartData(
-          pieTouchData: PieTouchData(
-            touchCallback: (FlTouchEvent event, pieTouchResponse) {
-              setState(() {
-                if (!event.isInterestedForInteractions ||
-                    pieTouchResponse == null ||
-                    pieTouchResponse.touchedSection == null) {
-                  touchedIndex = -1;
-                  return;
-                }
-                touchedIndex =
-                    pieTouchResponse.touchedSection!.touchedSectionIndex;
-              });
-            },
-          ),
-          borderData: FlBorderData(
-            show: false,
-          ),
-          sectionsSpace: 0,
-          centerSpaceRadius: 40,
-          sections: showingExpenseSections(transactionRecords, true),
-        ),
-      );
-    }
-    return content;
-  }
-
-  Widget buildIncomeStatisticsScreen(
-      List<TransactionRecord> transactionRecords) {
-    Widget content = const Center(child: Text("No records yet"));
-    if (transactionRecords
-        .where((transactionRecord) =>
-            transactionRecord.recordType == RecordType.income)
-        .isNotEmpty) {
-      content = PieChart(
-        PieChartData(
-          pieTouchData: PieTouchData(
-            touchCallback: (FlTouchEvent event, pieTouchResponse) {
-              setState(() {
-                if (!event.isInterestedForInteractions ||
-                    pieTouchResponse == null ||
-                    pieTouchResponse.touchedSection == null) {
-                  touchedIndex = -1;
-                  return;
-                }
-                touchedIndex =
-                    pieTouchResponse.touchedSection!.touchedSectionIndex;
-              });
-            },
-          ),
-          borderData: FlBorderData(
-            show: false,
-          ),
-          sectionsSpace: 0,
-          centerSpaceRadius: 40,
-          sections: showingExpenseSections(transactionRecords, false),
-        ),
-      );
-    }
-    return content;
-  }
-
-  List<PieChartSectionData> showingExpenseSections(
-      List<TransactionRecord> transactionRecords, bool isExpense) {
-    return List.generate(4, (index) {
-      final isTouched = index == touchedIndex;
-      final radius = isTouched ? 60.0 : 50.0;
-      switch (index) {
-        case 0:
-          return _cusromPieChartSection(
-              transactionRecords,
-              isExpense ? ExpenseCategory.food : IncomeCategory.freelance,
-              radius,
-              isTouched,
-              isExpense);
-        case 1:
-          return _cusromPieChartSection(
-              transactionRecords,
-              isExpense ? ExpenseCategory.travel : IncomeCategory.salary,
-              radius,
-              isTouched,
-              isExpense);
-        case 2:
-          return _cusromPieChartSection(
-              transactionRecords,
-              isExpense ? ExpenseCategory.leisure : IncomeCategory.investment,
-              radius,
-              isTouched,
-              isExpense);
-        case 3:
-          return _cusromPieChartSection(
-              transactionRecords,
-              isExpense ? ExpenseCategory.shopping : IncomeCategory.gift,
-              radius,
-              isTouched,
-              isExpense);
-        default:
-          throw Error();
-      }
-    });
-  }
-
-  PieChartSectionData _cusromPieChartSection(
-    List<TransactionRecord> transactionRecords,
-    Enum category,
-    double radius,
-    bool showTitle,
-    bool isExpense,
-  ) {
-    return PieChartSectionData(
-      color: categoryColors[category],
-      value: transactionRecords
-          .where((transactionRecord) => isExpense
-              ? transactionRecord.expenseCategory == category
-              : transactionRecord.incomeCategory == category)
-          .length
-          .toDouble(),
-      badgeWidget: Icon(categoryIcons[category]),
-      title: category.name,
-      titleStyle: Theme.of(context).textTheme.bodyLarge!.copyWith(),
-      titlePositionPercentageOffset: 1.5,
-      showTitle: showTitle,
-      radius: radius,
-    );
-  }
+class PieData {
+  PieData(this.category, this.value, [this.color]);
+  final String category;
+  final double value;
+  final Color? color;
 }
