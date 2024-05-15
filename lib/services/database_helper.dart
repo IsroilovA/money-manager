86ea386afaa4ac04bb740c30a5853a08086157ff
Alisher <aisroilov1029@gmail.com>
2024-05-15 20:30:48 +0500

    minor change

--- lib/services/database_helper.dart
+++ lib/services/database_helper.dart
@@ -67,7 +67,6 @@ class DatabaseHelper {
 
   static Future<void> editGoal(Goal goal) async {
     final db = await _openDB();
-    print(goal.goalBalance);
 
     await db
         .update("goals", goal.toJson(), where: 'id = ?', whereArgs: [goal.id]);
